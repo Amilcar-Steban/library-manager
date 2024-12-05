640f404a9b16fb92dfb0122faa351e6df1262954
Amilcar-Steban <amilcarshm@gmail.com>
2024-12-05 02:38:14 -0500

    add method calcularMultas

--- manager/src/model/Revista.java
+++ manager/src/model/Revista.java
@@ -1,20 +1,66 @@
 package model;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Revista extends ItemBiblioteca implements Catalogable {
 
     private int nroEdicion;
     private int cantidadEjemplares;
     private String nombreRevista;
+    private LocalDate fechaEntrega;
+    private final int TARIFA_MULTA_POR_DIA = 2500;
 
-    public Revista(int cantidadEjemplares, String nombreRevista, int nroEdicion) {
+    
+
+    public Revista(int nroEdicion, int cantidadEjemplares, String nombreRevista, LocalDate fechaEntrega) {
+        this.nroEdicion = nroEdicion;
         this.cantidadEjemplares = cantidadEjemplares;
         this.nombreRevista = nombreRevista;
-        this.nroEdicion = nroEdicion;
+        this.fechaEntrega = fechaEntrega;
     }
 
     public Revista() {
     }
 
+    /* OVERRIDE METHODS ABSTRACT AND INTERFACE */
+    @Override
+    public void prestar() {
+        if (cantidadEjemplares > 0) {
+            cantidadEjemplares--;
+            System.out.println("Listo!. Quedan " + cantidadEjemplares + " ejemplares.");
+        } else {
+            System.out.println("No hay stock disponible.");
+        }
+    }
+
+    @Override
+    public void devolver() {
+        cantidadEjemplares++;
+        System.out.println("Listo!. Hay " + cantidadEjemplares + " ejemplares disponibles.");
+    }
+
+    @Override
+    public void calcularMultas() {
+        LocalDate fechaActual = LocalDate.now();
+        if (fechaEntrega.isBefore(fechaActual)) {
+            long diasDeRetraso = ChronoUnit.DAYS.between(fechaEntrega, fechaActual);
+            double multa = diasDeRetraso * TARIFA_MULTA_POR_DIA; // Asumiendo que tienes una constante TARIFA_MULTA_POR_DIA
+            System.out.println("Multa actual: " + multa);
+        }
+        else {
+            System.out.println("Quedan "+(ChronoUnit.DAYS.between(fechaActual, fechaEntrega))+ " dias");
+        }
+    }
+
+    @Override
+    public void obtenerInfo() {
+        System.out.println("Revista: " + nombreRevista);
+        System.out.println("Número de Edición: " + nroEdicion);
+        System.out.println("Cantidad de Ejemplares: " + cantidadEjemplares);
+    }
+
+    /* GETTERS AND SETTERS */
     public int getNroEdicion() {
         return nroEdicion;
     }
@@ -39,31 +85,11 @@ public class Revista extends ItemBiblioteca implements Catalogable {
         this.nombreRevista = nombreRevista;
     }
 
-    @Override
-    public void prestar() {
-        if (cantidadEjemplares > 0) {
-            cantidadEjemplares--;
-            System.out.println("Listo!. Quedan " + cantidadEjemplares + " ejemplares.");
-        } else {
-            System.out.println("No hay stock disponible.");
-        }
+    public LocalDate getFechaEntrega() {
+        return fechaEntrega;
     }
 
-    @Override
-    public void devolver() {
-        cantidadEjemplares++;
-        System.out.println("Listo!. Hay " + cantidadEjemplares + " ejemplares disponibles.");
-    }
-
-    @Override
-    public void calcularMultas() {
-        System.out.println("El cálculo de multas no está implementado para revistas.");
-    }
-
-    @Override
-    public void obtenerInfo() {
-        System.out.println("Revista: " + nombreRevista);
-        System.out.println("Número de Edición: " + nroEdicion);
-        System.out.println("Cantidad de Ejemplares: " + cantidadEjemplares);
+    public void setFechaEntrega(LocalDate fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
     }
 }
--- manager/src/ui/App.java
+++ manager/src/ui/App.java
@@ -1,19 +1,20 @@
 package ui;
 
+import java.time.LocalDate;
 import model.Revista;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Revista[] revistas = new Revista[3];
+        LocalDate entregaFecha = LocalDate.of(2024, 12, 28);
 
-        revistas[0] = new Revista(5, "Ciencia Hoy", 101);
-        revistas[1] = new Revista(2, "Naturaleza", 202);
-        revistas[2] = new Revista(0, "Historia Viva", 303);
+        revistas[0] = new Revista(101, 5, "Ciencia Hoy", entregaFecha);
+        revistas[1] = new Revista(202, 2, "Naturaleza", entregaFecha);
+        revistas[2] = new Revista(303, 0, "Historia Viva", entregaFecha);
 
         for (Revista revista : revistas) {
             revista.obtenerInfo();
-            revista.prestar();
-            revista.devolver();
+            revista.calcularMultas();
             System.out.println();
         }
     }
