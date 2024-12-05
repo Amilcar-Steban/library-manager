89ebdf352f2eb7ec2ffdfcf11a3aa875b7687ce8
Amilcar-Steban <amilcarshm@gmail.com>
2024-12-05 00:57:28 -0500

    estructure + class Revista

---
+++ manager/src/model/ItemBiblioteca.java
@@ -0,0 +1,9 @@
+package model;
+
+public abstract class ItemBiblioteca {
+
+    public abstract void prestar();
+    public abstract void devolver();
+    public abstract void calcularMultas();
+
+}
---
+++ manager/src/model/Revista.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Revista extends ItemBiblioteca implements Catalogable{
+
+    @Override
+    public void prestar() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void devolver() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void calcularMultas() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void obtenerInfo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
