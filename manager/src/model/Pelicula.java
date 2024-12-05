d06bf2b47a18f1a64beaf200ad35054bf0ac7ab3
Amilcar-Steban <amilcarshm@gmail.com>
2024-12-05 01:05:29 -0500

    ajust in class Revista, modify prestar()

--- manager/src/model/Pelicula.java
+++ manager/src/model/Pelicula.java
@@ -2,43 +2,6 @@ package model;
 
 public class Pelicula extends ItemBiblioteca implements Catalogable{
     
-    private int nroEdicion;
-    private int cantidadEjemplares;
-    private String nombreRevista;
-
-    public Pelicula(int cantidadEjemplares, String nombreRevista, int nroEdicion) {
-        this.cantidadEjemplares = cantidadEjemplares;
-        this.nombreRevista = nombreRevista;
-        this.nroEdicion = nroEdicion;
-    }
-
-    public Pelicula() {
-    }
-
-    public int getNroEdicion() {
-        return nroEdicion;
-    }
-
-    public void setNroEdicion(int nroEdicion) {
-        this.nroEdicion = nroEdicion;
-    }
-
-    public int getCantidadEjemplares() {
-        return cantidadEjemplares;
-    }
-
-    public void setCantidadEjemplares(int cantidadEjemplares) {
-        this.cantidadEjemplares = cantidadEjemplares;
-    }
-
-    public String getNombreRevista() {
-        return nombreRevista;
-    }
-
-    public void setNombreRevista(String nombreRevista) {
-        this.nombreRevista = nombreRevista;
-    }
-
     @Override
     public void prestar() {
         throw new UnsupportedOperationException("Not supported yet.");
--- manager/src/model/Revista.java
+++ manager/src/model/Revista.java
@@ -2,9 +2,51 @@ package model;
 
 public class Revista extends ItemBiblioteca implements Catalogable{
 
+    private int nroEdicion;
+    private int cantidadEjemplares;
+    private String nombreRevista;
+
+    public Revista(int cantidadEjemplares, String nombreRevista, int nroEdicion) {
+        this.cantidadEjemplares = cantidadEjemplares;
+        this.nombreRevista = nombreRevista;
+        this.nroEdicion = nroEdicion;
+    }
+
+    public Revista() {
+    }
+
+    public int getNroEdicion() {
+        return nroEdicion;
+    }
+
+    public void setNroEdicion(int nroEdicion) {
+        this.nroEdicion = nroEdicion;
+    }
+
+    public int getCantidadEjemplares() {
+        return cantidadEjemplares;
+    }
+
+    public void setCantidadEjemplares(int cantidadEjemplares) {
+        this.cantidadEjemplares = cantidadEjemplares;
+    }
+
+    public String getNombreRevista() {
+        return nombreRevista;
+    }
+
+    public void setNombreRevista(String nombreRevista) {
+        this.nombreRevista = nombreRevista;
+    }
+
     @Override
     public void prestar() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (cantidadEjemplares<1) {
+            cantidadEjemplares--;
+        }else{
+            System.out.println("No hay stock, sorry");
+        }
+        
     }
 
     @Override
